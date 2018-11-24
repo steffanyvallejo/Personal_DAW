7940251b83182073a515a6f2d4f1693e5811fcf6
steffanyvallejo <stefyvallejo.vallejo@gmail.com>
2018-11-24 18:23:35 -0500

    Taller 2: formato terminado

--- Taller2-JS/script.js
+++ Taller2-JS/script.js
@@ -1,73 +1,111 @@
 /* Cargar las noticias de noticias.json y noticias.xml */
-/*function addText(texto){
- var label = $("<label/>", {
+var div;
+function addNew(autor, url, fecha, descripcion) {
+  
+  /*autor*/
+  var title = $("<p/>", {
+    "class": "card-title col-md-12 col-sm-12 col-12 ",
+    html: String(autor).bold()+", dijo: "
+
+  })
+
+  /*contenido*/
+  var p = $("<p />", {
+   
+    "class": "contenido card-title col-md-12 col-12  col-sm-12 py-0 py+0 mx+0 my ",
     
-    html: texto
-  })
-
-  var contenedor = $("<div/>", {
-    "class": "card-body card col-md-10 col-lg-8",
-  });
-
- label.appendTo(contenedor);
-
-}*/
-function addNew(autor, contenido, url, fecha, descripcion) {
-
-  var title = $("<h6/>", {
-    "class": "card-title col-md-6",
-    
-    html: a+" dijo:"
-  })
-
-  var p = $("<p/>", {
-    "class": "card-text col-md-6",
-    html: contenido
-  })
-  var image = $("<p/>", {
-    "class": "card-text col-md-6 ",
     html: descripcion
   })
 
-  var a = $("<a/>", {
-    "class": "card-text col-md-6",
+   p.addClass("contenido");
+
+  
+  /*enlace*/
+  var a = $("<a/>f", {
+    "class": "card-text col-md-12 col-12 col-sm-12",
     "href": url,
-    html: url
+    html: "Enlace al tweet"
   })
 
+  /*fecha*/
   var date = $("<p/>", {
-    "class": "card-text col-md-8 text-right",
+    "class": "card-text  col-md-12 col-12 text-right  col-sm-12 ",
     html: fecha
   })
+  date.addClass("fecha");
 
-  var div = $("<div/>", {
-    "class": "card-body card col-md-10 col-lg-8",
+  /*logo*/
+   var imagen = $("<img/>", {
+    "class": "card-text col-sm-12 col-md-12 col-12 img-fluid",
+    "src": "https://image.flaticon.com/icons/png/512/23/23681.png",
+    "style":"max-height:100px; max-width:100px; min-height:40px; min-width:50px",
+    html: imagen
+
+  })
+
+  /*div para la imagen*/
+  var i = $("<div/>", {
+    "class": "card-text col-md-2 col-sm-3 col-2"
   });
 
-  title.appendTo(div)
-  p.appendTo(div)
-  image.appendTo(div);
-  a.appendTo(div)
-  date.appendTo(div)
+/*contenedor de todos los tweets*/
+  div = $("<div/>", {
+    "class": "card-body  col-md-12 col-sm-12 col-12 row border-top px-2 pt-2 mx-0"
+
+  });
+  
+/*contenedor de un tweet*/
+  var formato = $("<div/>", {
+    "class": "card-text col-md-12 col-12 col-sm-12 row my-0"
+  });
+
+  /*contenedor de la segunda columna del tweet*/
+  var columnas = $("<div/>", {
+    "class": "card-text col-md-10 col-sm-9 col-10 px-0 px+0"
+  });
+
+  imagen.appendTo(i);
+  title.appendTo(columnas);
+  p.appendTo(columnas);
+  a.appendTo(columnas);
+  date.appendTo(columnas);
+  i.appendTo(formato);
+  columnas.appendTo(formato);
+  formato.appendTo(div);
+ 
+  
   div.appendTo("#tweets");
 }
 
 
 
-function loadNewsXml() {
+function loadTweetsXml() {
   $.ajax({
     type: "GET",
-    url: "rss.xml",
+    url: "https://twitrss.me/twitter_user_to_rss/?user=celex_espol",
     dataType: "xml",
     success: function (xml) {
       $(xml).find('item').each(function () {
-        var autor = $(this).find('dc:creator').text();
-        var contenido = $(this).find('title').text();
-        var descripcion = $(this).find('description');
-        var link = $(this).find('link');
-        var fecha = $(this).find('pubDate');
 
-        addNew(autor, contenido, link, fecha, descripcion)
+        /*autor del tweet*/
+        var autor = $(this).find('dc\\:creator').text();
+        var inicio = autor.indexOf("(");
+        var fin = autor.indexOf(")");
+        var username= autor.substring(inicio+1,fin);
+
+        /*contenido del tweet*/
+        var descripcion = $(this).find('description').text();
+
+        /*link del tweet*/
+        var url = $(this).find('link').text();
+        var link = String(url)
+
+        /*fecha del tweet*/
+        var date = $(this).find('pubDate').text();
+        var fecha = String(date).split(" ");
+        fecha = fecha[0] + " " + fecha[1] + " " + fecha[2] + " " + fecha[3] + " " + fecha[4];
+        addNew(username, link, fecha, descripcion)
+
 
 
       });
@@ -78,36 +116,38 @@ function loadNewsXml() {
   });
 }
 
-/* Filtrar las noticias de acuerdo al contenido ingresado en el input#buscador, ya sea por el texto en el título o en el contenido */
 
 $(document).ready(function () {
 
-
-  loadNewsXml();
-  loadNewsJson();
-
-
+loadTweetsXml();
   $("button").click(function (e) {
-
+     
     var texto = $('input#buscador').val();
-    addText($('input#buscador').val())
+
+
     if (texto.length != 0) {
-
-      var noticias = $('#tweets .card-body');
+     
+      document.getElementById('texto').innerHTML = texto;
+      
       $('#tweets .card-body').filter(function (index) {
-
+       
         $(this).show();
 
-        var noticia = $(this).text()
-        if (noticia.indexOf(texto) == -1) {
+        var t = $(this).text()
+        if (t.indexOf(texto) == -1) {
           $(this).hide()
+
         }
 
       });
 
     } else {
+      
       $('#tweets .card-body').each(function () {
-        $(this).show();
+        $(this).hide();
+        document.getElementById('texto').innerHTML = "";
+        
+        
       });
     } return false;
 
